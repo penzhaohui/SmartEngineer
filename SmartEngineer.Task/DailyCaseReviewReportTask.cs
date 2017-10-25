ea466def46ef59e34fc8f54b9253239bd82084ea
pen_zhaohui <pen_zhaohui@126.com>
2017-10-25 15:54:13 -0700

    上传SmartEngineer.Task项目

---
+++ SmartEngineer.Task/DailyCaseReviewReportTask.cs
@@ -0,0 +1,37 @@
+﻿using SmartTask;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SmartEngineer.Task
+{
+    public class DailyCaseReviewReportTask : AbstractTask
+    {
+        private static readonly Random rand = new Random();
+        protected static int RandSeconds()
+        {
+            return rand.Next(2000, 6000);
+        }
+
+        protected override TaskResult Work()
+        {
+            string message = "";
+            var stamp = TaskResultType.Succeed;
+            var rs = RandSeconds();
+            if (rs > 4500)
+            {
+                stamp = TaskResultType.Failed;
+                message = "Time out";
+            }
+
+            Thread.Sleep(rs);
+
+            Logger.Info("我执行了DailyCaseReviewReportTask的任务,工作了{0}秒, 结果是{1}", rs / 1000, stamp);
+
+            return new TaskResult() { Result = stamp, Message = message };
+        }
+    }
+}
---
+++ SmartEngineer.Task/ImportNewCaseTask.cs
@@ -0,0 +1,42 @@
+﻿using SmartTask;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SmartEngineer.Task
+{
+    public class ImportNewCaseTask : AbstractTask
+    {
+        private static readonly Random rand = new Random();
+        protected static int RandSeconds()
+        {
+            return rand.Next(2000, 6000);
+        }
+
+        protected override TaskResult Work()
+        {
+            string message = "";
+            var stamp = TaskResultType.Succeed;
+            var rs = RandSeconds();
+            if (rs > 4500)
+            {
+                stamp = TaskResultType.Failed;
+                message = "Time out";
+            }
+
+            Thread.Sleep(rs);
+
+            Logger.Info("我执行了ImportNewCaseTask的任务,工作了{0}秒, 结果是{1}", rs / 1000, stamp);
+
+            // 1, Get New Case List
+            // 2, Store New Case into local database
+            // 3, Import New Case into Jira
+            // 4, Store New Jira into local database
+
+            return new TaskResult() { Result = stamp, Message = message };
+        }
+    }
+}
