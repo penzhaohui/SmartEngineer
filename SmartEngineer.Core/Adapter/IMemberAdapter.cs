2571e0ad1930d339fb1c7e6cfdd17aaa46d6cac1
pen_zhaohui <pen_zhaohui@126.com>
2017-11-06 07:50:52 -0800

    上传漏掉的文件

---
+++ SmartEngineer.Core/Adapter/IMemberAdapter.cs
@@ -0,0 +1,53 @@
+﻿using SmartEngineer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartEngineer.Core.Adapter
+{
+    public interface IMemberAdapter
+    {
+        #region Tenant Operation
+        
+        List<Tenant> GetAllTenants();
+
+        Tenant GetTenantInfo(string name);
+
+        int SaveTenant(Tenant tenant);
+
+        #endregion
+
+        #region Member Operation
+
+        List<Member> GetAllMembers(Member memberSearchCriteria);
+
+        int SaveMember(Member member);
+
+        bool ActivateMember(string emailAddress, bool active);
+
+        bool ResetPasswrord(string emailAddress);
+
+        bool LinkToRole(string emailAddress, string role, bool isCancel);
+
+        bool LinkToGroup(string emailAddress, string group, bool isCancel);
+
+        #endregion
+
+        #region Role Operation
+
+        List<Role> GetAllRoles();
+
+        #endregion
+
+        #region Group Operation
+
+        List<Group> GetAllGroups();
+
+        #endregion
+
+        #region Permission Operation
+        #endregion
+    }
+}
